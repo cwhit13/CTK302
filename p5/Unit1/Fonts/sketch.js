835aafe18d9b9a93a946a0fe071c7d6e49f814fa
cwhit13 <54369159+cwhit13@users.noreply.github.com>
2020-09-08 22:34:00 -0500

    hrhfaef

--- p5/Unit1/Fonts/sketch.js
+++ p5/Unit1/Fonts/sketch.js
@@ -1,8 +1,10 @@
 var f;
+var f2;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1600, 1600);
   f = loadFont("Assets/AlfaSlabOne-Regular.ttf");
+    f2 = loadFont("Assets/cinzel.ttf");
   textAlign(CENTER);
 }
 
@@ -11,4 +13,7 @@ function draw() {
   textFont(f);
   textSize(68);
   text("Hello Darkness My Old Friend", width / 2, height / 2);
+  textFont(f2);
+  textSize(100);
+  text("I've come to talk to you", width / 2, height / 2 +80);
 }
