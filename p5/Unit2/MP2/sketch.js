4343eb5a18071511ecb8a0e5f46b4bc73b6129a8
cwhit13 <54369159+cwhit13@users.noreply.github.com>
2020-10-06 22:14:52 -0500

    lkfgdlg

--- p5/Unit2/MP2/sketch.js
+++ p5/Unit2/MP2/sketch.js
@@ -11,4 +11,30 @@ function draw() {
 
   image(img1, width/2, height/2, 400, 400);
 
+  switch (state) {
+    case 0:
+background ('red');
+text ("state 0", 250, 250);
+    break ;
+
+    case 1:
+    background ('blue');
+    text ("thicc ass boy", 250, 250);
+    break ;
+
+    case 2:
+    background ('purple');
+    text ("state 2", 250, 250);
+    break ;
+
+  }
+}
+
+function mouseReleased (){
+  state = state +1;
+  if (state >2) {
+    state = 0;
+  }
+}
+
 }
