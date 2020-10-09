cb81958631f74866dc0268efe1c549c19976a740
cwhit13 <54369159+cwhit13@users.noreply.github.com>
2020-10-08 21:03:02 -0500

    vjhvkh

--- p5/Unit2/MP2/sketch.js
+++ p5/Unit2/MP2/sketch.js
@@ -3,7 +3,7 @@ let state = 0;
 let song1;
 var mic;
 var vol;
-var c = 'yellow';
+var c = 'purple';
 
 function preload() {
   song1 = loadSound("assets/carl.mp3");
@@ -15,7 +15,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   mic = new p5.AudioIn();
   mic.start();
-  song1 = loadSound("assets/carl.")
+  //song1 = loadSound("assets/carl.mp3")
   img1 = loadImage("assets/img1.png")
   imageMode(CENTER);
 
@@ -32,11 +32,11 @@ function draw() {
   // check how loud the input is
   if (vol > .01) { // if the volume is LOUD?
     // do something
-  //  c = color(random(255), random(255), random(255)); // here I'm setting the background to a random color
+    //  c = color(random(255), random(255), random(255)); // here I'm setting the background to a random color
   }
   // extra stuff for debugging
   textSize(18);
-  text("Click the screen first to give\npermission for mic input.\nMy volume is " + vol, 10, 60);
+  //text("Click the screen first to give\npermission for mic input.\nMy volume is " + vol, 10, 60);
 
 
   switch (state) {
@@ -49,27 +49,29 @@ function draw() {
       break;
 
     case 1:
-    background("blue");
+      background("blue");
       image(img1, 500, 400, 612, 792);
       textSize(20);
       text("My name is Rex Morgan. If you yell, I will disappear.", 200, 50);
       if (vol > .08) {
-      state = 2;
+        state = 2;
       }
       break;
 
     case 2:
+    background('purple');
       text("Wanna hear my favorite song? \n Click on screen to play song", 200, 50);
       break;
 
-      case 3:
+    case 3:
       song1.play();
       state = 4;
       break;
 
-      case 4:
-        image(img1, 500, 400, 612, 792);
-        text ("Isn't this a bop?", 200, 50);
+    case 4:
+      background('purple');
+      image(img1, 500, 400, 612, 792);
+      text("Isn't this a bop?", 200, 50);
       break;
   }
 }
@@ -80,9 +82,10 @@ function touchStarted() {
 
 function mouseReleased() {
   state++;
-if (state > 4) {
-  state = 0;
-  song1.pause();}
+  if (state > 4) {
+    state = 0;
+    song1.pause();
+  }
 }
 
 // Code Freeze was originally 10/4 at 9pm. but you know how it be. Rex Morgan is the actor who plays Henry Higgins in My Fair Lady. What is his coorelation to a hippo? That is for the viewer to decide.
