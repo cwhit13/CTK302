513b62b27de9ecdd7ee3af1ae8702c581c764a76
cwhit13 <54369159+cwhit13@users.noreply.github.com>
2020-10-11 23:12:00 -0500

    lnlk

--- p5/Unit2/c1/sketch.js
+++ p5/Unit2/c1/sketch.js
@@ -1,62 +1,61 @@
 var x = 0;
 var mic;
 var vol;
-let velocity = 10;
-var c = rect
+//let velocity = 10; I thought I could plug in the code from the stoplight
+//project to make it work but it doesn't seem to be working :( :(
+//var c = rect
 
 function setup() {
   createCanvas(500, 500);
-    rect(200, 200, 50, 20);
+  rect(200, 200, 50, 20);
   rectMode(CENTER);
   mic = new p5.AudioIn();
   mic.start();
 }
 
 function draw() {
-  background(220);
-  rect(200, 200, 50, 20);
-  fill('purple');
-  x = x + velocity;
-  if (x > width)
-    x = 0;
-    vol = (mic.getLevel()).toFixed(2);
+  //background(220);
+  //rect(200, 200, 50, 20);
+  //fill('purple');
+  //x = x + velocity;
+  //if (x > width)
+  //  x = 0;
+  //  vol = (mic.getLevel()).toFixed(2);
 
-    if (vol > .5) {}
-    switch (state) {
+  //  if (vol > .5) {}
+  switch (state) {
 
-      case 0:
-        text("0", 100, 100);
-        velocity = 0;
-        vol = (mic.getLevel()).toFixed(2);
+    case 0:
+      background("yellow")
+      rect(x, 200, 50, 20);
+      fill('purple');
+      //velocity = 0;
+      vol = (mic.getLevel()).toFixed(2);
+      if (vol > .5) {
+        x += 10;
+        break;
+
         if (vol > .5) {
-          x += 10;
-          break;
-
-          if (vol > .5) {
-            x += 10;
-
-
-          }
+      x += 10;
         }
-    }
+      }
   }
 
-    if (w > width) x = 0;
+  if (w > width) x = 0;
+}
+//timer = timer + 1;
+//if (timer > 100) {
+  //timer = 0;
+//  state++;
+//  if (state > 2) state = 0;
 
-    timer = timer + 1;
-    if (timer > 100) {
-      timer = 0;
-      state++;
-      if (state > 2) state = 0;
-    }
 
+function touchStarted() {
+  getAudioContext().resume();
+}
 
-    function mouseReleased() {
-      state++;
-      if (state > 2) state = 0;
+function mouseReleased() {
+  state++;
+  if (state > 2) state = 0;
 
-    }
-
-    function touchStarted() {
-      getAudioContext().resume();
-    }
+}
