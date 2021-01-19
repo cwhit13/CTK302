ca369da105541ec56ed85d5da2639e7d3b9ac0ed
cwhit13 <54369159+cwhit13@users.noreply.github.com>
2021-01-19 16:47:56 -0600

    hkbhk

---
+++ p5/Unit5/birthday/sketch.js
@@ -0,0 +1,15 @@
+var f;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  f = loadFont ("assets/font.ttf");
+  textAlign (LEFT);
+}
+
+function draw() {
+  textFont(f);
+  textSize(40);
+  text("Yo, this is the top song for \n the day of August 25, 1997", 50, 100);
+  textSize(20);
+  text( "Mo Money, Mo Problems \n by the Notorious B.I.G. Featuring Puff Daddy & Mase" , 50, 200);
+
+}
